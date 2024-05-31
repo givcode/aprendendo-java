524d254fbbeb2a00cfb5de51cfe45002d008985d
givcode <gigs.santosp@gmail.com>
2024-05-30 23:40:45 -0300

    first commit

---
+++ src/pacoteTeste/PrimeiroJava.java
@@ -0,0 +1,35 @@
+package pacoteTeste;
+
+public class PrimeiroJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//	 int idade = 16;
+//	 String nome = "Giovanna";
+//	 
+//	 var nome2 = "Pedro";
+//	 var idade2 = 17;
+//	 
+//	 System.out.println("minha idade é: " + idade + " anos");	
+//	 System.out.println("meu nome é: " + nome);
+	
+//	String nome = "Pedro";
+//	String endereco = "Rua Jandira ";
+//	double salario = 2000.12;
+//	String dataRecebimentoSalario = "20/12/2020";
+//	String relatorio = "Eu " + nome + ", morando no endereço " 
+//			+ endereco + " \nconfirmo que recebi o salario de " + salario + ", na data " 
+//			+ dataRecebimentoSalario;
+//		
+//	System.out.println (relatorio);
+//		
+		
+		int numero1 = 10;
+	    double numero2 = 130.2;
+	    
+	    var adicao = numero1 + numero2;
+		
+	    System.out.println(adicao);
+
+}
